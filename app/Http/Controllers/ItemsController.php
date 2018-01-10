6c31a77c55f263cd942277ab816c8b043949a4f9
EmadBrown <emadbrown2013@gmail.com>
2018-01-10 20:11:24 +0100

    Add update and delete meothod item api

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -20,16 +20,6 @@ class ItemsController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -68,17 +58,6 @@ class ItemsController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -87,7 +66,22 @@ class ItemsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+      $validator = Validator::make($request->all(), [
+        'text' => 'required'
+    ]);
+
+    if($validator->fails()){
+       $response = array('response' => $validator->messages(), 'success' => false);
+       return $response;
+    }
+    else{
+      //create Item
+      $item =  Item::find($id);
+      $item->text = $request->input('text');
+      $item->body = $request->input('body');
+      $item->update();
+      return response()->json($item);
+    }
     }
 
     /**
@@ -98,6 +92,10 @@ class ItemsController extends Controller
      */
     public function destroy($id)
     {
-        //
+       $item =  Item::find($id);
+       $item->delete();
+       $response = array('response' => 'Item has deleted' , 'success' => true);
+       return $response;
+
     }
 }
